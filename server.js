a2cb503ac1053fb3d5b7577d66e48189a5b6e15d
James Gary <gmarchesi888@gmail.com>
2020-03-09 13:11:44 -0400

    Retrieving and parsing Pastebin data.

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const fetch = require("node-fetch");
+
 
 app.use(express.static("static"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+app.get("/data", (req, res) => {
+  fetch("https://pastebin.com/raw/BmA8B0tY").then(a =>
+    a.text().then(a => res.json(a.split("\r\n")))
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- static/index.js
+++ static/index.js
@@ -1,1 +1,8 @@
-console.log("Index.js here");
+console.log("Index.js here....");
+
+axios
+  .get("http://localhost:3000/data")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => console.error(error));
