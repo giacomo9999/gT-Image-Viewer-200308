67ba0eb9150cc4b55b6a13250157df2b14108540
James Gary <gmarchesi888@gmail.com>
2020-03-10 14:50:56 -0400

    Revised styling; added navbar (non-functional).

--- static/index.js
+++ static/index.js
@@ -1,10 +1,10 @@
-const image_gallery = document.querySelector(".image--container");
-const image_container = document.querySelector(".image--selection");
+const big_image = document.querySelector(".big-image-display");
+const thumbnail_grid = document.querySelector(".thumbnail-grid");
 const loading = `<h1 class="loader">Loading...</h1>`;
 
 const showImages = () => {
-  if (image_container.children.length === 0) {
-    image_container.innerHTML = loading;
+  if (thumbnail_grid.children.length === 0) {
+    thumbnail_grid.innerHTML = loading;
   }
   axios
     .get("http://localhost:3000/data")
@@ -16,26 +16,20 @@ const showImages = () => {
 
 const createImageGallery = images => {
   let output = "";
-  image_gallery.innerHTML = `<img src="${images[0]}" class="animate-entrance image--gallery" alt="image description">`;
-  setTimeout(() => {
-    image_gallery.children[0].classList.remove("animate-entrance");
-  }, 2000);
+  big_image.innerHTML = `<img src="${images[0]}" class="big-image" alt="image description">`;
   images.forEach(image => {
     output += `<img src="${image}" alt="Image" class="image__item"/>`;
   });
-  image_container.innerHTML = output;
+  thumbnail_grid.innerHTML = output;
 };
 
 const changeBigImage = e => {
-  const image = image_gallery.children[0];
+  console.log("Changing Image...", e.target.src);
+  const image = big_image.children[0];
   if (e.target.src) {
-    image.classList.add("animate-entrance");
     image.src = e.target.src;
-    setTimeout(() => {
-      image.classList.remove("animate-entrance");
-    }, 800);
   }
 };
 
 document.addEventListener("DOMContentLoaded", showImages);
-image_container.addEventListener("click", changeBigImage);
+thumbnail_grid.addEventListener("click", changeBigImage);
