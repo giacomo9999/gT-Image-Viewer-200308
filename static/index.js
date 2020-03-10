e97b32f5ace36dcb7fa764b59d550f8ba8b44222
James Gary <gmarchesi888@gmail.com>
2020-03-09 20:37:36 -0400

    Beginning to build 'display images' functionality.

--- static/index.js
+++ static/index.js
@@ -1,8 +1,24 @@
 console.log("Index.js here....");
 
-axios
-  .get("http://localhost:3000/data")
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => console.error(error));
+const image_gallery = document.querySelector("image--container");
+const image_container = document.querySelector(".image--selection");
+const loading = `<h1 class="loader">Loading...</h1>`;
+
+const showImages = () => {
+  if (image_container.children.length === 0) {
+    image_container.innerHTML = loading;
+  }
+  axios
+    .get("http://localhost:3000/data")
+    .then(imageUrlArray => {
+      console.log("Data in...", imageUrlArray.data);
+      createImageGallery(imageUrlArray.data);
+    })
+    .catch(error => console.error(error));
+};
+
+const createImageGallery = images => {
+  console.log("Create Image Gallery here with the data...", images);
+};
+
+document.addEventListener("DOMContentLoaded", showImages);
